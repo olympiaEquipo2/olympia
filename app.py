cfc3ebcd3b64fb036ac6211cfb32086f258bc679
romina rodes <rominarodes@gmail.com>
2023-11-23 15:30:13 -0300

    metodo mostrar usuario y registrarse en clase usuario

--- app.py
+++ app.py
@@ -22,10 +22,6 @@ CORS(app) # Esto habilitará CORS para todas las rutas
 
 
 class Usuario:
-    """
-    Esta clase proporciona métodos para administrar un catálogo de tareas
-    almacenados en una base de datos MySQL.
-    """
     #----------------------------------------------------------------
     # Constructor de la clase
     def __init__(self, host, user, password, database):
@@ -49,99 +45,83 @@ class Usuario:
 
         self.conn.commit()
         self.cursor.close()
+        self.cursor = self.conn.cursor(dictionary=True)
 #-------------------------------------------------------------
 # Si no existe crea la tabla usuarios
-        self.cursor = self.conn.cursor(dictionary=True)
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS `{database}`.`usuarios` (
-                    `id_usuario` INT NOT NULL AUTO_INCREMENT,
-                    `nombre` VARCHAR(50) NOT NULL,
-                    `apellido` VARCHAR(50) NOT NULL,
-                    `email` VARCHAR(200) NOT NULL,
-                    `contraseña` VARCHAR(8) NOT NULL,
-                    PRIMARY KEY (`id_usuario`),
-                    UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);''')
+        # self.cursor = self.conn.cursor(dictionary=True)
+        # self.cursor.execute('''CREATE TABLE IF NOT EXISTS `{database}`.`usuarios` (
+        #             `id_usuario` INT NOT NULL AUTO_INCREMENT,
+        #             `nombre_completo` VARCHAR(30) NOT NULL,
+        #             `apellido` VARCHAR(30) NOT NULL,
+        #             `correo_electronico` VARCHAR(30) NOT NULL,
+        #             `contraseña` VARCHAR(11) NOT NULL,
+        #             PRIMARY KEY (`id_usuario`),
+        #             UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);''')
+        # self.conn.commit()
+        # self.cursor.close()
+
+        # self.cursor = self.conn.cursor(dictionary=True)
+
+
+    def mostrar_usuario(self, id_usuario):
+        sql = f"SELECT * FROM usuarios where id_usuario = {id_usuario}"
+        self.cursor.execute(sql)
+        usuario = self.cursor.fetchone()
         self.conn.commit()
-        self.cursor.close()
-
-        self.cursor = self.conn.cursor(dictionary=True)
-#----------------------------------------------------------------
-#Si no existe crea la tabla cursos
-        self.cursor = self.conn.cursor(dictionary=True)
-        self.cursor.execute('''CREATE TABLE `{database}`.`cursos` (
-                    `id_curso` INT NOT NULL AUTO_INCREMENT,
-                    `nombre` VARCHAR(45) NOT NULL,
-                    `descripcion` VARCHAR(500) NOT NULL,
-                    `dias` VARCHAR(45) NOT NULL,
-                    `horario` VARCHAR(45) NOT NULL,
-                    PRIMARY KEY (`id_curso`));''')
-        self.conn.commit()
-        self.cursor.close()
-
-        self.cursor = self.conn.cursor(dictionary=True)
-#----------------------------------------------------------------
-#Si no existe crea la tabla cursos_usuarios
-        self.cursor = self.conn.cursor(dictionary=True)
-        self.cursor.execute('''CREATE TABLE `olympia`.`cursos_usuario` (
-                    `idcursos_usuario` INT NOT NULL,
-                    `fk_usuario` INT NOT NULL,
-                    `fk_cursos` INT NOT NULL,
-                    PRIMARY KEY (`idcursos_usuario`),
-                    INDEX `fk_usuario_idx` (`fk_usuario` ASC) VISIBLE,
-                    INDEX `fk_curso_idx` (`fk_cursos` ASC) VISIBLE,
-                    CONSTRAINT `fk_usuario`
-                        FOREIGN KEY (`fk_usuario`)
-                        REFERENCES `olympia`.`usuarios` (`id_usuario`)
-                        ON DELETE CASCADE
-                        ON UPDATE CASCADE,
-                    CONSTRAINT `fk_curso`
-                        FOREIGN KEY (`fk_cursos`)
-                        REFERENCES `olympia`.`cursos` (`id_curso`)
-                        ON DELETE CASCADE
-                        ON UPDATE CASCADE);''')
-        self.conn.commit()
-        self.cursor.close()
-
-        self.cursor = self.conn.cursor(dictionary=True)
-
+        if usuario:
+            return usuario
+        else:
+            return False
 
     def registrarse(self, nombre, apellido, email, contraseña):
-        self.cursor.execute(f"SELECT * FROM usuarios WHERE email = {email}")
+        self.cursor.execute(f"SELECT * FROM usuarios WHERE correo_electrónico = '{email}'")
         usuario_existe = self.cursor.fetchone()
         if usuario_existe:
             return False
 
-        sql = "INSERT INTO usuarios (nombre,apellido,email,contraseña) VALUES (%s, %s, %s, %s)"
+        sql = "INSERT INTO usuarios (nombre_completo,apellido,correo_electrónico,contraseña) VALUES (%s, %s, %s, %s)"
         valores = (nombre,apellido,email,contraseña)
 
-        self.cursor.execute(sql, valores)        
+        self.cursor.execute(sql, valores)
         self.conn.commit()
         return True
 
-  
+
 #----------------------------------------------------------------
-@app.route("/", methods=["GET"])      
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/actividades", methods=["GET"])      
+@app.route("/actividades", methods=["GET"])
 def actividades():
     return render_template("actividades.html")
 
-@app.route("/nosotros", methods=["GET"])      
+@app.route("/nosotros", methods=["GET"])
 def nosotros():
     return render_template("nosotros.html")
 
-@app.route("/contacto", methods=["GET"])      
+@app.route("/contacto", methods=["GET"])
 def contacto():
     return render_template("contacto.html")
 
-@app.route("/registrarse", methods=["GET"])      
+@app.route("/registrarse", methods=["GET"])
 def registrarse():
     return render_template("registrarse.html")
 
-#usuario = Usuario(host='localhost', user='root', password='', database='olympia')
+usuario = Usuario(host='localhost', user='root', password='', database='olympia')
+
+
+#----------------PRUEBAS---------------------------------------------4
+
+print(usuario.mostrar_usuario(1))
+print(usuario.registrarse("miguel", "vincent", "mm@h.com", 1234566666))
+
+
+
+
+
 
 #--------------------------------------------------------------------
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#         app.run(debug=True)
 #--------------------------------------------------------------------
